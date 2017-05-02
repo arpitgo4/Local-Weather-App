0bee37c93e4e7c9743cd053c1401e84fbdde87e1
Arpit Goyal <arpit.go4@gmail.com>
2017-05-02 15:46:11 +0530

    animation in sun component added

--- src/components/Footer.component.js
+++ src/components/Footer.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Footer extends React.Component {
 	render() {
 		return (
-			<div className="row">
+			<div className="row footer">
 				<div className="col-xs-12 text-center">
 					<p>
 						Inspired By <a href="javascript:void(0)">Arpit Goyal</a>
--- src/components/Sun.component.js
+++ src/components/Sun.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Sun extends React.Component {
 	render() {
 		return (
-			<div className="row">
+			<div className="row sun">
 				<div className="col-xs-12 text-center">
 					<i className="wi wi-day-sunny" />
 				</div>
